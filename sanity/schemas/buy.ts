9f083a334f471215bd5ee2013091fc13c0e7a71d
Ademayowa <ademayowaadeniyi@gmail.com>
2023-09-13 11:10:29 +0100

    updated schema fields

--- sanity/schemas/buy.ts
+++ sanity/schemas/buy.ts
@@ -29,7 +29,7 @@ export default defineType({
     defineField({
       name: 'description',
       title: 'Description',
-      type: 'string',
+      type: 'text',
     }),
     defineField({
       name: 'author',
